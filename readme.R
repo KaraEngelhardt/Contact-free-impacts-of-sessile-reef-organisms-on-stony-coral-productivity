0747a0a2ba91d28af8d27e48efa2c9b76f1ae27b
Kara <kara.engelhardt@freenet.de>
2023-09-12 15:00:46 +0200

    read me improved

--- readme.R
+++ readme.R
@@ -1,5 +1,10 @@
 # readme
 
+
+# This repository belongs to the paper 'Contact-free impacts of sessile reef organisms on stony coral productivity' by Engelhardt et al. 2023 
+# published in Communications Earth & Environment and contains all raw data generated in the experiments, 
+# as well as the scripts to perform the statistical analyses and generate the graphs. The work was done in R statistical environment (v.4.0.2, R Core Team 2020).
+
 # order of scripts for experiment 1&2
 
 #Experiment 1
